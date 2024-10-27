d9879c4b109123c85d683916f30c9210c3fb56b9
Josimar Da Silva Rodrigues <josimarfifo2016@gmail.com>
2024-10-26 23:36:28 -0300

    aplicativo de sorteio

---
+++ app/src/main/java/com/example/desafiojosimar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.desafiojosimar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.function.IntConsumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    public void sortearNumero (View view ){
+        TextView texto = findViewById(R.id.textView2);
+        int x = new Random().nextInt(11);
+        texto.setText("Número:"+x);
+    }
+
+}
